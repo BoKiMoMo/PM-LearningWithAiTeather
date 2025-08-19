cfedb0ba4c03df121443cb22d3d6c6b8fab217d9
BoKiMoMo <aacd870624@gmail.com>
2025-08-19 17:12:35 +0800

    day22

--- week04/Day22/Day22_Practice.cpp
+++ week04/Day22/Day22_Practice.cpp
@@ -3,7 +3,22 @@
 #include <iostream>
 using namespace std;
 
+class Car {
+public:
+    string brand;   // 屬性 / Attribute
+    int year;       // 屬性 / Attribute
+
+    void honk() {   // 方法 / Method
+        cout << brand << " says: Beep! Beep!" << endl;
+    }
+};
+
 int main() {
-    // 實作從這裡開始
+    Car myCar;
+    myCar.brand = "Toyota";
+    myCar.year = 2022;
+
+    cout << "Brand: " << myCar.brand << ", Year: " << myCar.year << endl;
+    myCar.honk();
     return 0;
 }
